96ce087f9342a7ba29e39b503077a1a4db0debf4
maksofer-dev <73745873+maksofer-dev@users.noreply.github.com>
2023-06-16 08:06:25 +0400

    Add files via upload

---
+++ AddCounterAgent.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RJD_IntangibleValuesAccounting.View
+{
+    /// <summary>
+    /// Логика взаимодействия для AddDisciplineWindow.xaml
+    /// </summary>
+    public partial class AddCounterAgent : Window
+    {
+        private CounterAgent _agent = new CounterAgent();
+        public AddCounterAgent(CounterAgent selectedCounterAgent)
+        {
+            InitializeComponent();
+            if (selectedCounterAgent != null)
+            {
+                addCounterAgent.Title = "РЖД - Изменение информации о контрагенте";
+                _agent = selectedCounterAgent;
+
+            }
+            else
+            {
+                addCounterAgent.Title = "РЖД - Добавление контрагента";
+
+            }
+            DataContext = _agent;
+
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_agent.CounterAgentName))
+                errors.AppendLine("Укажите название организации");
+            if (string.IsNullOrWhiteSpace(_agent.CounterAgentHolder))
+                errors.AppendLine("Укажите владельца");
+            if (string.IsNullOrWhiteSpace(_agent.CounterAgentAddress))
+                errors.AppendLine("Укажите адресс организации");
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_agent.CounterAgentID == 0)
+            {
+                IntangibleAssetsDBEntities.GetContext().CounterAgents.Add(_agent);
+            }
+            try
+            {
+                IntangibleAssetsDBEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+                addCounterAgent.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void cancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        
+
+    }
+}
---
+++ CounterAgentListWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RussianCosmetics.View
+{
+    /// <summary>
+    /// Логика взаимодействия для CounterAgentWindow.xaml
+    /// </summary>
+    public partial class CounterAgentListWindow : Window
+    {
+        public CounterAgentListWindow()
+        {
+            InitializeComponent();
+            
+            dGridAgents.ItemsSource = IntangibleAssetsDBEntities.GetContext().CounterAgents.ToList();
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddCounterAgent addCounterAgent = new AddCounterAgent(null);
+            addCounterAgent.ShowDialog();
+
+        }
+
+        private void backBN_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenuWindow mainMenu = new MainMenuWindow(userID, roleID);
+            mainMenu.Show();
+            counterAgentWindow.Close();
+
+        }
+
+        private void delBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var agentsForRemoving = dGridAgents.SelectedItems.Cast<CounterAgent>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить следующие {agentsForRemoving.Count()} элементов?", "Внимание",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    IntangibleAssetsDBEntities.GetContext().CounterAgents.RemoveRange(agentsForRemoving);
+                    IntangibleAssetsDBEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены!");
+                    dGridAgents.ItemsSource = IntangibleAssetsDBEntities.GetContext().CounterAgents.ToList();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void refreshBtn_Click(object sender, RoutedEventArgs e)
+        {
+            IntangibleAssetsDBEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+            dGridAgents.ItemsSource = IntangibleAssetsDBEntities.GetContext().CounterAgents.ToList();
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            AddCounterAgent editAgentWindow = new AddCounterAgent((sender as Button).DataContext as CounterAgent);
+            editAgentWindow.ShowDialog();
+        }
+
+        private void screenBN_Click(object sender, RoutedEventArgs e)
+        {
+            CreateScreenShotFromVisual(dGridAgents);
+        }
+    }
+}
